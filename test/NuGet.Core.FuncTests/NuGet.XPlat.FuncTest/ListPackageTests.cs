a432324399654644b684087ee5e937b1ee2c1df9
Joel Verhagen <joel.verhagen@gmail.com>
2020-05-23 18:33:49 -0700

    Add tests

--- test/NuGet.Core.FuncTests/NuGet.XPlat.FuncTest/ListPackageTests.cs
+++ test/NuGet.Core.FuncTests/NuGet.XPlat.FuncTest/ListPackageTests.cs
@@ -1,6 +1,7 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Threading.Tasks;
@@ -19,43 +20,76 @@ namespace NuGet.XPlat.FuncTest
         [Fact]
         public void BasicListPackageParsing_Interactive()
         {
-            // Arrange
-            using (var testDirectory = TestDirectory.Create())
-            {
-                var projectPath = Path.Combine(testDirectory, "project.csproj");
-                File.WriteAllText(projectPath, string.Empty);
+            VerifyCommand(
+                (projectPath, mockCommandRunner, testApp) =>
+                {
+                    // Arrange
+                    var argList = new List<string> { "list", "--interactive", projectPath };
 
-                var argList = new List<string>() {
-                    "list",
-                    "--interactive",
-                    projectPath};
+                    // Act
+                    var result = testApp.Execute(argList.ToArray());
 
-                var logger = new TestCommandOutputLogger();
-                var testApp = new CommandLineApplication();
-                var mockCommandRunner = new Mock<IListPackageCommandRunner>();
-                mockCommandRunner
-                    .Setup(m => m.ExecuteCommandAsync(It.IsAny<ListPackageArgs>()))
-                    .Returns(Task.CompletedTask);
-
-                testApp.Name = "dotnet nuget_test";
-                ListPackageCommand.Register(testApp,
-                    () => logger,
-                    () => mockCommandRunner.Object);
-
-                // Act
-                var result = testApp.Execute(argList.ToArray());
-
-                XPlatTestUtils.DisposeTemporaryFile(projectPath);
-
-                // Assert
-                mockCommandRunner.Verify();
-                Assert.NotNull(HttpHandlerResourceV3.CredentialService);
-                Assert.Equal(0, result);
-            }
+                    // Assert
+                    mockCommandRunner.Verify();
+                    Assert.NotNull(HttpHandlerResourceV3.CredentialService);
+                    Assert.Equal(0, result);
+                });
         }
 
         [Fact]
         public void BasicListPackageParsing_InteractiveTakesNoArguments()
+        {
+            VerifyCommand(
+                (projectPath, mockCommandRunner, testApp) =>
+                {
+                    // Arrange
+                    var argList = new List<string>() { "list", "--interactive", "no", projectPath };
+
+                    // Act & Assert
+                    Assert.Throws<CommandParsingException>(() => testApp.Execute(argList.ToArray()));
+                });
+        }
+
+        [Fact]
+        public void BasicListPackageParsing_ShowProtocolLogs()
+        {
+            VerifyCommand(
+                (projectPath, mockCommandRunner, testApp) =>
+                {
+                    // Arrange
+                    var argList = new List<string> { "list", projectPath, "--show-protocol-logs" };
+
+                    // Act
+                    var result = testApp.Execute(argList.ToArray());
+
+                    // Assert
+                    mockCommandRunner.Verify(
+                        x => x.ExecuteCommandAsync(It.Is<ListPackageArgs>(a => a.ShowProtocolLogs)),
+                        Times.Once);
+                    Assert.Equal(0, result);
+                });
+        }
+
+        [Fact]
+        public void BasicListPackageParsing_NoShowProtocolLogs()
+        {
+            VerifyCommand((projectPath, mockCommandRunner, testApp) =>
+                {
+                    // Arrange
+                    var argList = new List<string> { "list", projectPath };
+
+                    // Act
+                    var result = testApp.Execute(argList.ToArray());
+
+                    // Assert
+                    mockCommandRunner.Verify(
+                        x => x.ExecuteCommandAsync(It.Is<ListPackageArgs>(a => !a.ShowProtocolLogs)),
+                        Times.Once);
+                    Assert.Equal(0, result);
+                });
+        }
+
+        private void VerifyCommand(Action<string, Mock<IListPackageCommandRunner>, CommandLineApplication> verify)
         {
             // Arrange
             using (var testDirectory = TestDirectory.Create())
@@ -63,12 +97,6 @@ namespace NuGet.XPlat.FuncTest
                 var projectPath = Path.Combine(testDirectory, "project.csproj");
                 File.WriteAllText(projectPath, string.Empty);
 
-                var argList = new List<string>() {
-                    "list",
-                    "--interactive",
-                    "no",
-                    projectPath};
-
                 var logger = new TestCommandOutputLogger();
                 var testApp = new CommandLineApplication();
                 var mockCommandRunner = new Mock<IListPackageCommandRunner>();
@@ -81,8 +109,15 @@ namespace NuGet.XPlat.FuncTest
                     () => logger,
                     () => mockCommandRunner.Object);
 
-                // Act
-                Assert.Throws<CommandParsingException>(() => testApp.Execute(argList.ToArray()));
+                // Act & Assert
+                try
+                {
+                    verify(projectPath, mockCommandRunner, testApp);
+                }
+                finally
+                {
+                    XPlatTestUtils.DisposeTemporaryFile(projectPath);
+                }
             }
         }
     }
